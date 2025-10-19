5f520927dc9817629b792a80fa6931fb9be566e4
Tymofii <snop.0904@gmail.com>
2025-10-20 01:47:08 +0200

    Part B: +2 поля, миграции, API, UI

--- src/main/java/com/example/crudproduct/Product.java
+++ src/main/java/com/example/crudproduct/Product.java
@@ -33,7 +33,6 @@ public class Product {
         this.createdAt = createdAt;
     }
 
-    // getters/setters
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
